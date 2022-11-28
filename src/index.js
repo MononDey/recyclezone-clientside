8a16c5df6d0c5aa80d03d1a76cc2235fbafe6afe
Monon Kanti Dey <mononkanti@gmail.com>
2022-11-28 13:54:49 +0600

    6

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AuthProvider from '../src/context/AuthProvider';
+
 import {
   QueryClient,
   QueryClientProvider,
